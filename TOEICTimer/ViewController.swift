95b6b1645fe3d0b58d36b3b542e1ef7aecdc3b7f
pinebook <i.applepie0416@gmail.com>
2019-06-26 23:55:17 +0900

    Initial Commit

---
+++ TOEICTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TOEICTimer
+//
+//  Created by 松本将輝 on 2019/06/26.
+//  Copyright © 2019 masaki.matsumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
